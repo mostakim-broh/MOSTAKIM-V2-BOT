f5444aaedc8d5ee7ce576f3347b071fe0c2afc83
mostakim-sagor <mostakim.sagor.69@gmail.com>
2026-05-08 22:07:57 +0600

    first commit

--- utils/log.js
+++ utils/log.js
@@ -1,29 +1,40 @@
 const chalk = require('chalk');
 
-module.exports = (data, option) => {
+module.exports = (data, option = "LOG") => {
 	switch (option) {
+
 		case "warn":
-			console.log(chalk.yellow('[ ❕ ] » ') + data);
+			console.log(chalk.yellow('[ WARNING ] » ') + data);
 			break;
+
 		case "error":
-			console.log(chalk.red('[ ❕ ] » ') + data);
+			console.log(chalk.red('[ ERROR ] » ') + data);
 			break;
+
+		case "success":
+			console.log(chalk.green('[ SUCCESS ] » ') + data);
+			break;
+
 		default:
-			console.log(chalk.magenta(`${option} » `) + data);
+			console.log(chalk.magenta(`[ ${option.toUpperCase()} ] » `) + data);
 			break;
 	}
 }
 
 module.exports.loader = (data, option) => {
+
 	switch (option) {
+
 		case "warn":
-			console.log(chalk.yellow('[ LOADER ] » ') + data);
+			console.log(chalk.yellow('[ LOADER WARNING ] » ') + data);
 			break;
+
 		case "error":
-			console.log(chalk.red('[ LOADER ] » ') + data);
+			console.log(chalk.red('[ LOADER ERROR ] » ') + data);
 			break;
+
 		default:
-			console.log(chalk.green(`[ LOADER ] » `) + data);
+			console.log(chalk.green('[ MOSTAKIM V2 BOT LOAD ] » ') + data);
 			break;
 	}
 }
